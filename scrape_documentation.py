0e8f690f6101c21da0105b9ff18d8f5fcebd4bdc
Justin <justin.bellavance@gmail.com>
2025-01-12 17:25:35 -0500

    added python2, did % of total words instead

--- scrape_documentation.py
+++ scrape_documentation.py
@@ -6,31 +6,69 @@ def main():
     
     versions = []
     num_mentions = []
+    # version 2
+    for minor in range(0, 8):
+        n_error = 0
+        version = "2." +  str(minor)
+
+        page = requests.get(f"https://docs.python.org/2/whatsnew/{version}.html")
+        
+        soup = BeautifulSoup(page.text, "html.parser")
+        
+        # Extract all text from the page
+        text = soup.get_text()
+
+        # Split the text into words and count them
+        words = text.split()
+
+        # Count the total number of words
+        word_count = len(words)
+
+        results_err = soup.find_all(string=re.compile("error", re.IGNORECASE))
+        # results_exc = soup.find_all(string=re.compile("exception", re.IGNORECASE))
+
+        n_error += len(results_err)
+        # n_error += len(results_exc)
+        
+        print(version, round(n_error / word_count * 100, 2))
+        versions.append(version)
+        num_mentions.append(round(n_error / word_count * 100, 2))
+    
+    # version 3
     for minor in range(0, 14):
         n_error = 0
         version = "3." +  str(minor)
 
         page = requests.get(f"https://docs.python.org/3/whatsnew/{version}.html")
         
-        soup = BeautifulSoup(page.content, "html.parser")
+        soup = BeautifulSoup(page.text, "html.parser")
+        
+        # Extract all text from the page
+        text = soup.get_text()
+
+        # Split the text into words and count them
+        words = text.split()
+
+        # Count the total number of words
+        word_count = len(words)
 
         results_err = soup.find_all(string=re.compile("error", re.IGNORECASE))
-        results_exc = soup.find_all(string=re.compile("exception", re.IGNORECASE))
+        # results_exc = soup.find_all(string=re.compile("exception", re.IGNORECASE))
 
         n_error += len(results_err)
-        n_error += len(results_exc)
+        # n_error += len(results_exc)
         
-        print(version, n_error)
+        print(version, round(n_error / word_count * 100, 2))
         versions.append(version)
-        num_mentions.append(n_error)
+        num_mentions.append(round(n_error / word_count * 100, 2))
             
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(14, 8))
     plt.style.use("fivethirtyeight")
     
     plt.bar(versions, num_mentions)
     
     plt.xlabel("Python 3 Versions")
-    plt.ylabel("Mentions of either 'error' or 'exception'")  
+    plt.ylabel("'error' mentions per word (%)")  
     
     plt.subplots_adjust(left=0.15, right=1, top=0.85, bottom=0.15)
     plt.savefig("p3_err.png", bbox_inches='tight')
